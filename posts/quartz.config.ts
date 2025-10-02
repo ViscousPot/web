a50320ec56a7f5a2eb3a405436dbc2345f89d7af
ViscousPot <viscouspotential@gmail.com>
2025-10-02 20:45:57 +0100

    Update quartz.config.ts

--- posts/quartz.config.ts
+++ posts/quartz.config.ts
@@ -116,7 +116,7 @@ const config: QuartzConfig = {
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
+      Plugin.CustomOgImages({colorScheme: "darkMode"}),
     ],
   },
 }
